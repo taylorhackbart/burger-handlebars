0fda25ff396a9366c5cd6e8d77ebd0336c7dac7e
Taylor <taylor.hackbart@du.edu>
2020-11-03 15:26:55 -0800

    heroku connection

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,16 @@
 var mysql = require("mysql");
 
+if(process.env.JAWSDB_URL){
+  connection= mysql.createConnection(process.env.JAWSDB_URL)
+} else {
 var connection = mysql.createConnection({
   host: "localhost",
-  port: 3306,
+  port: process.env.PORT || 3306,
   user: "root",
   password: "taylor123!",
   database: "burgers_db",
 });
+}
 
 connection.connect(function (err) {
   if (err) {
