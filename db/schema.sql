94b5e43abaf7a05aa54e9edc5bcdd6f2d588e94d
Taylor <taylor.hackbart@du.edu>
2020-11-03 07:05:37 -0800

    variable name changes

--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,7 @@ USE burgers_db;
 CREATE TABLE burgers (
   id INT AUTO_INCREMENT NOT NULL,
   burger_name VARCHAR(30) NOT NULL,
-  devoured BOOLEAN NOT NULL,
+  devour BOOLEAN,
   PRIMARY KEY (id)
 );
 
